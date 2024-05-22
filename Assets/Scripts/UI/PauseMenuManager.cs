48f99fe5a1c90bde7171e66b7902e086d7cb2894
Colin Spiridonov <colinspiri@gmail.com>
2024-05-22 15:29:16 -0700

    added pause menu

---
+++ Assets/Scripts/UI/PauseMenuManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenuManager : MonoBehaviour {
+    public static PauseMenuManager Instance;
+
+    [SerializeField] private GameObject pauseMenu;
+    private PlayerInputActions _inputActions;
+    
+    private bool _paused;
+    
+    private void Start()
+    {
+        if (Instance)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+
+        _inputActions = new PlayerInputActions();
+        _inputActions.Enable();
+        
+        ClosePauseMenu();
+    }
+    
+    private void Update()
+    {
+        if (_paused) return;
+        
+        if (_inputActions.UI.Cancel.triggered)
+        {
+            if (pauseMenu.activeSelf) ClosePauseMenu();
+            else OpenPauseMenu();
+        }
+    }
+
+    private void OpenPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        Pause();
+    }
+
+    public void ClosePauseMenu()
+    {
+        pauseMenu.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
+
+        Resume();
+    }
+    
+    private void Pause() {
+        Time.timeScale = 0;
+    }
+
+    private void Resume() {
+        Time.timeScale = 1;
+    }
+}
