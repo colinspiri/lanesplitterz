3643196cfaa7d66a248e41d1534529264f55fe25
DGM <melendez.pro.email@gmail.com>
2024-06-03 12:13:50 -0800

    IcyField and Billboard prefab changes
    Also cuz github wants me to push so UI prefab doesnt get overwritten. (Currently working on speedometer)

--- Assets/Scripts/icyField.cs
+++ Assets/Scripts/icyField.cs
@@ -7,13 +7,13 @@ public class icyField : MonoBehaviour
     //sends a log to console if ball is in icy field (might also do the same if any OTHER collider interacts with it, but, ya know)
     private void OnTriggerEnter( Collider collider )
     {      
-        Debug.Log("Icy Field");
+        //Debug.Log("Icy Field");
     }
 
     //draws a gizmo over the icy field
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position, new Vector3(69, 1, 24));
+        //Gizmos.DrawWireCube(transform.position, new Vector3(69, 1, 24));
     }
 
 
