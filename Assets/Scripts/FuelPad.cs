c0d063352abd85bfbca5a6879a490e2ed6ce295d
DGM <melendez.pro.email@gmail.com>
2024-06-04 15:49:34 -0800

    Added FuelPad obstacle prefab
    title

---
+++ Assets/Scripts/FuelPad.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelPad : MonoBehaviour
+{
+    private PlayerMovement _playerMove;
+    [Range(0.01f, 1f)]
+    [SerializeField] private float fuelAdd = 0.1f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _playerMove = GameObject.FindWithTag("Player")?.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter()
+    {
+        _playerMove.RestoreFuel( fuelAdd );
+    }
+}
