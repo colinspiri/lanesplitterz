4bba5d828ce469f5134fd637552dc370fc6c247e
Colin Spiridonov <colinspiri@gmail.com>
2024-06-20 15:11:46 -0700

    fuel meter only appears when fuel used

--- Assets/Scripts/UI/FuelMeter.cs
+++ Assets/Scripts/UI/FuelMeter.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ScriptableObjectArchitecture;
 
-public class FuelMeter : Meter
-{
+public class FuelMeter : Meter {
+    [SerializeField] private float remainTime = 1;
+    private float _remainTimer;
+    
     public override void Start()
     {
         base.Start();
@@ -13,5 +16,17 @@ public class FuelMeter : Meter
     public void UpdateFuelMeter(FloatVariable fuel)
     {
         slider.value = fuel.Value;
+        
+        EnableMeter();
+        _remainTimer = remainTime;
+    }
+
+    private void Update() {
+        if (_remainTimer > 0) {
+            _remainTimer -= Time.deltaTime;
+        }
+        else {
+            DisableMeter();
+        }
     }
 }
