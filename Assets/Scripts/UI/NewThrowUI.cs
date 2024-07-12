7d301b371de1d52ccbf7f574d0d169b76df0ab57
Brooklyn Humphrey <69836100+BrooklynBHumphrey@users.noreply.github.com>
2024-07-12 11:58:47 -0700

    add waiting area UI

--- Assets/Scripts/UI/NewThrowUI.cs
+++ Assets/Scripts/UI/NewThrowUI.cs
@@ -10,7 +10,15 @@ public class NewThrowUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI roundText;
     [SerializeField] private float roundUITime;
     [SerializeField] private GameEvent roundUIEnd;
+    [SerializeField] private IntVariable playerCurrentPoints;
+    [SerializeField] private IntVariable enemyCurrentPoints;
+    [SerializeField] private FloatVariable levelResetTime; // use this in Waiting Area script
+    [SerializeField] private GameObject clearingPinsUI;
+    [SerializeField] private GameObject currentScoresUI;
+    [SerializeField] private TextMeshProUGUI currentScoresText;
+
     private int currentRound = 0;
+    private int currentThrow = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -33,4 +41,35 @@ public class NewThrowUI : MonoBehaviour
             if (currentRound == 1) roundUIEnd.Raise(); // should start dialogue
         }
     }
+
+    private IEnumerator DisplayClearingPinsUI()
+    {
+        if (currentThrow % 2 == 1)
+        {
+            clearingPinsUI.SetActive(true);
+            yield return new WaitForSeconds(levelResetTime.Value);
+            clearingPinsUI.SetActive(false);
+            currentThrow++;
+        }
+    }
+
+    private IEnumerator DisplayCurrentScores()
+    {
+        RoundManager.Instance.playerPointsByThrow.Add(playerCurrentPoints.Value);
+        RoundManager.Instance.enemyPointsByThrow.Add(enemyCurrentPoints.Value);
+
+        if (currentThrow % 2 == 0)
+        {
+            RoundManager.Instance.CalculateFinalScores();
+            currentScoresText.text = "Player current score: " + RoundManager.Instance.playerFinalScore + "\nEnemy current score: " + RoundManager.Instance.enemyFinalScore;
+            currentScoresUI.SetActive(true);
+            yield return new WaitForSeconds(levelResetTime.Value);
+            currentScoresUI.SetActive(false);
+            currentThrow++;
+        }
+    }
+
+    public void CallDisplayClearingPins() => StartCoroutine(DisplayClearingPinsUI());
+
+    public void CallDisplayCurrentScores() => StartCoroutine(DisplayCurrentScores());
 }
