88dc12bcf346b95bdb4c788cced77ff9eb893dd3
HenryGossGrubbs <123278438+HenryGossGrubbs@users.noreply.github.com>
2024-07-20 13:38:10 -0700

    new elvis vo sound

--- Assets/VOPlayer.cs
+++ Assets/VOPlayer.cs
@@ -1,12 +1,18 @@
 using GameAudioScriptingEssentials;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Yarn.Unity;
 
 public class VOPlayer : MonoBehaviour
 {
     [SerializeField] AudioClipRandomizer ProtagVO;
+    [SerializeField] AudioClipRandomizer ElvisVO;
+    [SerializeField] TextMeshProUGUI characterName;
+    [SerializeField] string elvisName;
     public int frequency = 2;
+    private bool swing = false;
     private int stop;
 
     private void Start()
@@ -15,6 +21,26 @@ public class VOPlayer : MonoBehaviour
     }
     public void PlayCharacterVO()
     {
+        if (characterName.text == elvisName)
+        {
+            if (stop == 1)
+            {
+                ElvisVO.PlaySFX();
+                if (swing == false)
+                {
+                    stop = frequency * 4;
+                }
+                else
+                {
+                    stop = frequency * 2;
+                }
+                swing = !swing;
+            }
+            else
+            {
+                stop--;
+            }
+        }
         if (stop == 1)
         {
             ProtagVO.PlaySFX();
