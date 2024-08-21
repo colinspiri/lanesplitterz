407176e58c735e262d94d969c0fea10fa2eeb2f8
jsaqib <83430876+jsaqib@users.noreply.github.com>
2024-08-20 23:16:47 -0700

    double pin prefab and mine model

---
+++ Assets/Art/3D Art/Obstacles/Mines/WSM Game Studio/Landmine/Scripts/ExplosionController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WSMGameStudio.Bombs
+{
+    public class ExplosionController : MonoBehaviour
+    {
+        public float power = 10f;
+        public float radius = 2f;
+        public float upForce = 1f;
+        public int damage = 10;
+
+        private void Start()
+        {
+            Explode();
+            Destroy(gameObject, 1.5f);
+        }
+
+        // Update is called once per frame
+        void Explode()
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(power, transform.position, radius, upForce, ForceMode.Impulse);
+
+                    //Handle Explosion Damage here
+                    //Sample
+                    //Health healthScript = hit.gameObject.GetComponent<Health>();
+
+                    //if (healthScript != null)
+                    //    healthScript.TakeDamage(damage);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Art/3D Art/Obstacles/Mines/WSM Game Studio/Landmine/Scripts/Landmine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WSMGameStudio.Bombs
+{
+    public class Landmine : MonoBehaviour
+    {
+        public GameObject explosionPrefab;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Explode();
+        }
+
+        private void Explode()
+        {
+            GameObject explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+    } 
+}
