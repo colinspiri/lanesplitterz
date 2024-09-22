7d5c19abfea6f2729e44c9c49df4bcb1381d47e8
Brooklyn Humphrey <69836100+BrooklynBHumphrey@users.noreply.github.com>
2024-09-22 16:54:46 -0700

    Clean time manager code

--- Assets/Scripts/Managers/TimeManager.cs
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -6,8 +6,6 @@ public class TimeManager : MonoBehaviour
 {
     public static TimeManager Instance;
     
-    // private float _fixedDeltaTime;
-
     public bool pausedForTutorial;
 
     private Stack<float> _timeScales;
@@ -21,14 +19,12 @@ public class TimeManager : MonoBehaviour
         }
         Instance = this;
         
-        // _fixedDeltaTime = Time.fixedDeltaTime;
         _timeScales = new Stack<float>();
     }
 
     private void SetTimeScaleUnstored(float newTimeScale)
     {
         Time.timeScale = newTimeScale;
-        // Time.fixedDeltaTime = _fixedDeltaTime * Time.timeScale;
     }
 
     // sets timescale while ignoring tutorial pauses - use carefully!
@@ -65,22 +61,18 @@ public class TimeManager : MonoBehaviour
 
     public void PauseTime()
     {
-        Time.timeScale = 0f;
-
-        //SetTimeScaleUnsafe(0);
+        SetTimeScaleUnsafe(0);
     }
 
     public void ResumeTime()
     {
-        Time.timeScale = 1f;
-
-        //if (_timeScales.Count == 0)
-        //{
-        //    SetTimeScaleUnstored(1);
-        //    return;
-        //}
-        //float previousTimeScale = _timeScales.Pop();
-        //SetTimeScaleUnstored(previousTimeScale);
+        if (_timeScales.Count == 0)
+        {
+            SetTimeScaleUnstored(1);
+            return;
+        }
+        float previousTimeScale = _timeScales.Pop();
+        SetTimeScaleUnstored(previousTimeScale);
     }
 
     private void OnDestroy()
