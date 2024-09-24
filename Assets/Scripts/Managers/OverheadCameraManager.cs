fd0f9b7d609abb1380e2b435a1685c73efc0b507
Colin Spiridonov <colinspiri@gmail.com>
2024-09-24 13:18:02 -0700

    added overhead camera, can be toggled with TAB

---
+++ Assets/Scripts/Managers/OverheadCameraManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class OverheadCameraManager : MonoBehaviour {
+
+    [SerializeField] private GameObject overheadCamera;
+    [SerializeField] private GameObject playerCamera;
+
+    [SerializeField] private GameState gameState;
+
+    private bool _ballLaunched;
+    private bool _overheadViewEnabled;
+
+    private void Start() {
+        overheadCamera.SetActive(false);
+        
+        RoundManager.OnNewThrow += Initialize;
+        RoundManager.OnNewRound += Initialize;
+    }
+
+    private void Initialize() {
+        _ballLaunched = false;
+        _overheadViewEnabled = false;
+        SetOverheadCamera(_overheadViewEnabled);
+    }
+
+    private void Update() {
+        if (gameState.isDialogueRunning || _ballLaunched) return;
+
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            SetOverheadCamera(!_overheadViewEnabled);
+        }
+    }
+
+    private void SetOverheadCamera(bool value) {
+        _overheadViewEnabled = value;
+
+        overheadCamera.SetActive(_overheadViewEnabled);
+        playerCamera.SetActive(!_overheadViewEnabled);
+    }
+
+    // called from LaunchedBall GameEvent SO
+    public void NotifyBallLaunched() {
+        _ballLaunched = true;
+        SetOverheadCamera(false);
+    }
+}
