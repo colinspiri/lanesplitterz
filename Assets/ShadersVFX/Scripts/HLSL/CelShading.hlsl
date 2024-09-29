d0540f7f1440eaa2c173241df2485677875126e3
PhuongPhamUSC <phuongp@usc.edu>
2024-09-28 18:19:09 -0700

    updated cel shading and material toon

--- Assets/ShadersVFX/Scripts/HLSL/CelShading.hlsl
+++ Assets/ShadersVFX/Scripts/HLSL/CelShading.hlsl
@@ -3,11 +3,13 @@
 
 void CelShading_float(float luminance, out float Out)
 {
-    if (luminance > 0.0f && luminance <= 0.25f)
+    if (luminance >= 0.0f && luminance < 0.25f)
+        Out = 0.0f;
+    else if (luminance >= 0.25f && luminance < 0.5f)
         Out = 0.25f;
-    else if (luminance > 0.25f && luminance <= 0.5f)
+    else if (luminance >= 0.5f && luminance < 0.75f)
         Out = 0.5f;
-    else if (luminance > 0.5f && luminance <= 0.75f)
+    else if (luminance >= 0.75f && luminance < 1.0f)
         Out = 0.75f;
     else
         Out = 1.0f;
