5d2b3e0a4508a9b586244003a105f5e96bed83c8
Brooklyn Humphrey <69836100+BrooklynBHumphrey@users.noreply.github.com>
2025-01-26 16:26:06 -0800

    Add ability to skip opening cutscene with any key press or mouse click

--- Assets/Scripts/Managers/VideoManager.cs
+++ Assets/Scripts/Managers/VideoManager.cs
@@ -7,12 +7,28 @@ using UnityEngine.Video;
 public class VideoManager : MonoBehaviour
 {
     private VideoPlayer videoPlayer;
+    private bool skippedCutscene = false;
     // Start is called before the first frame update
     void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
         videoPlayer.loopPointReached += LoadMainMenu;
         videoPlayer.Play();
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown && !skippedCutscene)
+        {
+            skippedCutscene = true;
+            SkipCutscene();
+        }
+    }
+
+    private void SkipCutscene()
+    {
+        videoPlayer.Stop();
+        LoadMainMenu(videoPlayer);
     }
 
     private void LoadMainMenu(VideoPlayer vp)
